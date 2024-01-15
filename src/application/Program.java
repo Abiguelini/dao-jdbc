0c27890ab3133e1ef34e62eaf71677493e315f9a
Abiguelini <114603454+Abiguelini@users.noreply.github.com>
2024-01-15 13:28:34 -0300

    Entidade Departamento criada

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import db.model.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department dp = new Department(1, "null");
+		System.out.println(dp);
+	}
+
+}

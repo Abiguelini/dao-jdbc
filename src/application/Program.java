9343b4e634e258530a6c5cad53be20a13dff84e3
Abiguelini <114603454+Abiguelini@users.noreply.github.com>
2024-01-15 17:06:43 -0300

    Entidade Departamento criada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import db.model.Department;
+import db.model.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department dp = new Department(1, "null");
+		Department dp = new Department(1, "Livros");
 		System.out.println(dp);
+		Seller seller = new Seller(21, "bob", "bob@gmailcom", new  Date(), 2100.00, dp);
+		System.out.println(seller);
 	}
 
 }
